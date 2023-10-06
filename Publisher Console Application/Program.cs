4ff94e27bafcdfd320a4278d938f540520565d2e
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-10-07 01:26:31 +0530

    Adding Redis pubsub

---
+++ Publisher Console Application/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisPublisher
+{
+    class Program
+    {
+        private static string message;
+
+        static void Main(string[] args)
+        {
+            using (var redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var pubsub = redis.GetSubscriber();
+
+                while (true)
+                {
+                    Console.WriteLine("Enter topic to publish to (or 'exit' to quit):");
+                    var topic = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(topic) || topic == "exit") break;
+
+                    Console.WriteLine($"Publishing to topic '{topic}'. Enter message (or 'back' to change topic, 'exit' to quit):");
+                    while (true)
+                    {
+                        var message = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(message) || message == "back" || message == "exit") break;
+
+                        pubsub.Publish(topic, message);
+                        Console.WriteLine($"Message '{message}' published to '{topic}'. Enter another message or 'back' to change topic:");
+                    }
+
+                    if (message == "exit") break;
+                }
+            }
+        }
+    }
+}
---
+++ Subscriber Console Application/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var pubsub = redis.GetSubscriber();
+
+                Console.WriteLine("Enter topics you want to subscribe to (comma-separated, e.g., 'topic1,topic2'):");
+                var topicsInput = Console.ReadLine();
+                var topics = topicsInput.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var topic in topics)
+                {
+                    pubsub.Subscribe(topic.Trim(), (channel, message) =>
+                    {
+                        Console.WriteLine($"Received message on {channel}: {message}");
+                    });
+
+                    Console.WriteLine($"Subscribed to {topic.Trim()}");
+                }
+
+                Console.WriteLine("Listening for messages. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
