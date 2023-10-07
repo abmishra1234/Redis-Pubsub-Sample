367aa547492809d5b081d8bf06ac82c3bb7ce6c2
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-10-07 12:35:17 +0530

    updating the missing files

---
+++ RedisStreamPublisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisStreamPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var db = redis.GetDatabase();
+
+                while (true)
+                {
+                    Console.WriteLine("Enter message (or 'exit' to quit):");
+                    var message = Console.ReadLine();
+
+                    if (message == "exit") break;
+
+                    // We're using "mystream" as the stream name. This can be modified.
+                    db.StreamAdd("mystream", "message", message);
+                }
+            }
+        }
+    }
+}
