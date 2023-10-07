3a9161cbbb1b080e3b523188ee13b3a14a8364d3
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-10-07 12:23:57 +0530

    Adding STream Example as Sample

---
+++ Redis Stream Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisStreamSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var db = redis.GetDatabase();
+
+                // Initial reading can be set to start from the oldest message.
+                var position = StreamPosition.Beginning;
+
+                while (true)
+                {
+                    // Reading the stream
+                    var messages = db.StreamRead("mystream", position);
+
+                    foreach (var message in messages)
+                    {
+                        Console.WriteLine($"Received message: {message.Values[0].Value}");
+                        position = message.Id; // Setting the position to the last read message ID.
+                    }
+
+                    System.Threading.Thread.Sleep(1000); // Pausing for a second before the next read.
+                }
+            }
+        }
+    }
+}
